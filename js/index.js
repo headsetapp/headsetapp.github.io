1b1e2bf77b624d7478a38d3a338206f554cf3864
Daniel Ravina <raabhim@gmail.com>
2018-01-13 16:39:26 -0800

    new design ready

--- js/index.js
+++ js/index.js
@@ -3,13 +3,15 @@
 const styles = require('../css/style.scss');
 const $ = require('jquery');
 const MobileDetect = require('mobile-detect');
-const current_tag = require('./tag.txt')
-const md = new MobileDetect(window.navigator.userAgent);
 
-const handleDownloadLinks = () => {
+const md = new MobileDetect(window.navigator.userAgent);
+const axios = require('axios')
+const LATEST_RELEASE = 'https://api.github.com/repos/headsetapp/headset-electron/releases/latest'
+
+const handleDownloadLinks = (version) => {
   const os = window.navigator.userAgent
   const baseUrl = "https://github.com/headsetapp/headset-electron/releases/download"
-  const releaseUrl = `https://github.com/headsetapp/headset-electron/releases/tag/v${current_tag}`
+  const releaseUrl = `https://github.com/headsetapp/headset-electron/releases/tag/v${version}`
   const downloadsWrapper = $('.download')
   let download;
   let links = '';
@@ -18,23 +20,23 @@ const handleDownloadLinks = () => {
     download = {
       name: 'Windows 7/8/10',
       links: [
-        { filename: 'HeadsetSetup.exe', label: 'Headset.exe', tag: current_tag }
+        { filename: 'HeadsetSetup.exe', label: 'Headset.exe', version  }
       ]
     }
   } else if (os.indexOf('Mac') !== -1) {
     download = {
       name: 'macOS',
       links: [
-        { filename: `Headset-${current_tag}.dmg`, label: '.dmg', tag: current_tag},
-        { filename: `Headset.${current_tag}_mac.zip`, label: '.zip', tag: current_tag}
+        { filename: `Headset-${version}.dmg`, label: '.dmg', version },
+        { filename: `Headset-${version}.zip`, label: '.zip', version }
       ]
     }
   } else {
     download = {
       name: 'Linux',
       links: [
-        { filename: `headset_${current_tag}_amd64.deb`, label: '.deb', tag: current_tag},
-        { filename: `headset-${current_tag}.x86_64.rpm`, label: '.rpm', tag: current_tag}
+        { filename: `headset_${version}_amd64.deb`, label: '.deb', version },
+        { filename: `headset-${version}.x86_64.rpm`, label: '.rpm', version }
       ]
     }
   }
@@ -42,18 +44,25 @@ const handleDownloadLinks = () => {
   $('.os').text(download.name)
 
   download.links.forEach((link) => {
-    links += `<a class="primary-button" href="${baseUrl}/v${link.tag}/${link.filename}">${link.label}</a>`
+    links += `<a class="primary-button" href="${baseUrl}/v${link.version}/${link.filename}">${link.label}</a>`
   })
 
   $('.download-buttons').html(links).find('a').click((c) => {
-    ga('send', 'event', 'Download', current_tag);
+    ga('send', 'event', 'Download', version);
   })
 
   // e.g https://github.com/headsetapp/headset-electron/releases/tag/v1.2.3
   $('#release-link').attr('href', releaseUrl)
 }
 
+const getLatestTag = () => {
+  return axios.get(LATEST_RELEASE).then((response) => {
+    return response.data.tag_name;
+  })
+}
 
 $(document).ready(() => {
-  handleDownloadLinks();
+  getLatestTag().then((tag) => {
+    handleDownloadLinks(tag.replace('v', ''));
+  })
 });
